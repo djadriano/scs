e91a74e776a5486191927c1c3352507541f2c223
Adriano Fernandes <djadrianof@gmail.com>
2012-04-02 23:53:18 -0300

    update collection url

--- public/_/javascripts/_app/collections/search.js
+++ public/_/javascripts/_app/collections/search.js
@@ -1,4 +1,4 @@
 SCS.Collections.Search = Backbone.Collection.extend({
   model : SCS.Models.Search,
-  url   : 'http://api.soundcloud.com/tracks.json?client_id=d614588ae977c9792c5211ef5ca27945&linked_partitioning=1&limit=20&offset=0&order=created_at'
+  url   : 'http://api.soundcloud.com/tracks.json?client_id=d614588ae977c9792c5211ef5ca27945&linked_partitioning=1'
 });
