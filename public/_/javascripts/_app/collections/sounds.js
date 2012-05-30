821696ea2b4e00c46c3927bd9336b03d4f81d789
Adriano Fernandes <djadrianof@gmail.com>
2012-05-30 01:14:35 -0300

    add order results in home for hotness

--- public/_/javascripts/_app/collections/sounds.js
+++ public/_/javascripts/_app/collections/sounds.js
@@ -1,4 +1,4 @@
 SCS.Collections.Sounds = Backbone.Collection.extend({
   model : SCS.Models.Sounds,
-  url   : 'http://api.soundcloud.com/tracks.json?client_id=d614588ae977c9792c5211ef5ca27945&linked_partitioning=1&limit=20&offset=0&filter=all'
+  url   : 'http://api.soundcloud.com/tracks.json?client_id=d614588ae977c9792c5211ef5ca27945&linked_partitioning=1&limit=20&offset=0&filter=all&order=hotness'
 });
